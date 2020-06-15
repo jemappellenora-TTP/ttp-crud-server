324608532e982b33b6eb42f003dd604d62de8f82
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-15 00:40:38 -0400

    student route

--- database/models/student.js
+++ database/models/student.js
@@ -5,11 +5,11 @@ const Student = db.define("student", {
   firstName: { type: Sequelize.STRING, allowNull: false },
   lastName: { type: Sequelize.STRING, allowNull: false },
   email: { type: Sequelize.STRING, allowNull: false, validate: {isEmail: true} },
+  gpa: { type: Sequelize.FLOAT, validate:{isNumeric: true, min:0, max:4}},
   imageUrl: {
     type: Sequelize.STRING,
-    defaultValue: "https://via.placeholder.com/480x240?text=Placeholder",
+    defaultValue: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRWAjTO9sDIgksF8zbkTZ7rjZqqwJmYqKEJhcemmfYkwFn3kkz&usqp=CAU",
   },
-  gpa: { type: Sequelize.FLOAT, validate:{isNumeric: true, min:0, max:4}}
 });
 
 module.exports = Student;
--- routes/students.js
+++ routes/students.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
     const { id } = req.params;
     try {
-      const student = await Students.findByPk(id, { include:Campus });
+      const student = await Student.findByPk(id, { include:Campus });
       res.status(200).json(student);
     } catch (err) {
       next(err);
@@ -37,5 +37,42 @@ router.post("/", async (req, res, next) => {
       next(err);
     }
   });
+  
+  //route to edit students
+  //api/students/:id
+  router.put("/:id", async (req, res, next) => {
+    const { id } = req.params;
+    const {firstName,lastName,email,gpa,campusId } = req.body;
+    const updatedStudent= {
+        firstName,
+        lastName,
+        email,
+        gpa,
+        campusId
+    };
+    try {
+      const student = await Student.findByPk(id);
+      await student.set(updatedStudent);
+      const updatedInfo = await student.save();
+      res.status(201).send(updatedInfo);
+    } catch (err) {
+      next(err);
+    }
+  });
+  
+  // Route to handle removing 
+  // /api/students/:id
+  router.delete("/:id", async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const student = await Student.findByPk(id);
+      await student.destroy();
+      // send a success message to the client
+      res.sendStatus(204);
+    } catch (err) {
+      next(err);
+    }
+  });
+  
 
 module.exports = router;
